8db530b811dc3882ee60cd8772c0bd5df4017bb8
Andrew Schoen <schoen.andrewj@gmail.com>
2021-01-27 12:31:09 -0600

    fixed issues

--- tf2_web_republisher_py/tf2_web_republisher_py/tf2_web_republisher_py.py
+++ tf2_web_republisher_py/tf2_web_republisher_py/tf2_web_republisher_py.py
@@ -1,31 +1,32 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionServer
-from tf2_web_republisher_msgs.msg import TFArray
-from tf2_web_republisher_msgs.srv import RepublishTFs
-from tf2_web_republisher_msgs.action import TFSubscription
+from rclpy.callback_groups import ReentrantCallbackGroup
+from tf2_web_republisher.msg import TFArray
+from tf2_web_republisher.srv import RepublishTFs
+from tf2_web_republisher.action import TFSubscription
 from geometry_msgs.msg import TransformStamped
 from tf2_web_republisher_py.tf_pair import TFPair
-from tf2_ros import TransformException
+from tf2_ros import Buffer, TransformListener, TransformException
 
 from typing import List
 from uuid import uuid4
 
 class ClientInfo(object):
-    def __init__(self,client_id:int,tf_subscriptions:List[TFPair]=[]):
+    def __init__(self,client_id:int,tf_subscriptions:List[TFPair]=[],timer=None):
         self.client_id = client_id
         self.tf_subscriptions = tf_subscriptions
+        self.timer = timer
 
 class ClientGoalInfo(ClientInfo):
-    def __init__(self,handle,client_id:int,tf_subscriptions:List[TFPair]=[]):
-        super(ClientGoalInfo,self).__init__(client_id,tf_subscriptions)
+    def __init__(self,handle,client_id:int,tf_subscriptions:List[TFPair]=[],timer=None):
+        super(ClientGoalInfo,self).__init__(client_id,tf_subscriptions,timer)
         self.handle = handle
 
 class ClientRequestInfo(ClientInfo):
     def __init__(self,client_id:int,tf_subscriptions:List[TFPair]=[],publisher=None,timer=None,timeout:float=10.0):
-        super(ClientRequestInfo,self).__init__(client_id,tf_subscriptions)
+        super(ClientRequestInfo,self).__init__(client_id,tf_subscriptions,timer)
         self.publisher = publisher
-        self.timer = timer
         self.timeout = timeout
         self.last_time_since_subscribers = 0
 
@@ -34,21 +35,33 @@ class TFRepublisher(Node):
     Python port of the TFRepublisher C++ Node for ROS2
     '''
     def __init__(self):
-        self.tf_transform_server = ActionServer(self,
-                                                TFSubscription,
-                                                'tf2_web_republisher',
-                                                goal_callback=self.goal_cb,
+        super(TFRepublisher,self).__init__('tf2_web_republisher')
+        self.tf_transform_server = ActionServer(node=self,
+                                                action_type=TFSubscription,
+                                                action_name='tf2_web_republisher',
+                                                execute_callback=self.goal_cb,
                                                 cancel_callback=self.cancel_cb)
         self.republish_service = self.create_service(RepublishTFs, 'republish_tfs', self.request_cb)
-        self.tf_buffer = tf2_ros.Buffer()
-        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
-        self.active_requests = {}
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer,node=self)
+        self.active_clients = {}
 
     def goal_cb(self,goal_handle):
-        pass
+        goal = goal_handle.request
+        client_id = str(uuid4())
+        goal_info = ClientGoalInfo(goal_handle,client_id)
+        self.set_subscriptions(goal_info,
+                               goal.source_frames,
+                               goal.target_frames,
+                               goal.angular_thres,
+                               goal.trans_thres)
+        goal_info.timer = self.create_timer(1.0/goal.rate,lambda: self.process_goal(client_id))
+        self.active_clients[client_id] = goal_info
 
     def cancel_cb(self,goal_handle):
-        pass
+        for client_id, goal_info in self.active_clients.items():
+            if goal_handle == goal_info.handle:
+                self.teardown_client(client_id)
 
     def request_cb(self,request,response):
         self.get_logger().info("RepublishTF service request received");
@@ -65,7 +78,7 @@ class TFRepublisher(Node):
                                request.trans_thres)
         request_info.timeout = request.timeout
         request_info.timer = self.create_timer(1.0/request.rate,lambda: self.process_request(client_id))
-        self.active_requests[client_id] = request_info
+        self.active_clients[client_id] = request_info
         result.topic_name = topic_name
         self.get_logger().info('Publishing requested TFs on topic {0}'.format(topic_name))
         return response
@@ -77,21 +90,27 @@ class TFRepublisher(Node):
         return frame_id
 
     def set_subscriptions(self,client_info:ClientInfo,source_frames:List[str],target_frame:str,angular_thres:float,trans_tres:float):
-        pass
+        client_info.tf_subscriptions = []
+        for source_frame in source_frames:
+            tf_pair = TFPair(self.clean_tf_frame(source_frame),self.clean_tf_frame(target_frame),angular_thres,trans_thres)
+            client_info.tf_subscriptions.append(tf_pair)
 
-    def teardown_request(self,client_id):
-        request = self.active_requests[client_id]
-        request.timer.destroy()
-        request.publisher.destroy()
-        del self.active_requests[client_id]
+    def teardown_client(self,client_id):
+        client = self.active_clients[client_id]
+        client.timer.destroy()
+        if isinstance(client,ClientRequestInfo):
+            client.publisher.destroy()
+        elif isinstance(client,ClientGoalInfo):
+            client.handle.succeed()
+        del self.active_clients[client_id]
 
     def process_request(self, client_id:str):
-        request = self.active_requests[client_id]
+        request = self.active_clients[client_id]
         # Check whether the publisher has any subscribers. If it does, update the stored time.
         if request.pub.get_subscription_count() > 0:
             request.last_time_since_subscribers = self.get_clock().now()
         elif self.get_clock().now() - request.last_time_since_subscribers > request.timeout:
-            self.teardown_request(client_id)
+            self.teardown_client(client_id)
             return
         # Continue processing the request
         tf_array = self.update_subscriptions(request.tf_subscriptions)
@@ -101,6 +120,20 @@ class TFRepublisher(Node):
             request.publisher.publish(TFArray(transforms=tf_array))
         else:
             self.get_logger().debug('Request {0} No TF frame update needed:'.format(request.client_id))
+
+    def process_goal(self, client_id:str):
+        goal = self.active_clients[client_id]
+        feedback = goal.handle.Feedback()
+
+        # Continue processing the request
+        tf_array = self.update_subscriptions(request.tf_subscriptions)
+        if len(tf_array) > 0:
+            # publish TFs
+            self.get_logger().debug('Client {0} TFs feedback published:'.format(request.client_id))
+            feedback.transforms = TFArray(transforms=tf_array)
+            goal.handle.publish_feedback(feedback)
+        else:
+            self.get_logger().debug('Client {0}: No TF frame update needed:'.format(request.client_id))
 
     def update_subscriptions(self,tf_subscriptions:List[TFPair]) -> List[TransformStamped]:
         transforms = []
@@ -118,7 +151,7 @@ class TFRepublisher(Node):
                 # update tf_pair with transformtion
                 pair.update_transform(transform)
 
-            except TransformException as e:
+            except Exception as e:
                 if pair.is_ok:
                     # Only log an error if the transform was okay before
                     self.get_logger().warning(str(e))
